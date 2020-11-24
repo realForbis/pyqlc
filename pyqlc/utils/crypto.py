a16189b9fb680226f0203529aba7c29dca5a1a71
Forbis <37193612+realForbis@users.noreply.github.com>
2020-11-24 20:47:33 +0100

    Update crypto.py

--- pyqlc/utils/crypto.py
+++ pyqlc/utils/crypto.py
@@ -1,12 +1,18 @@
-import string
 import struct
 from base64 import b32encode, b32decode
 from hashlib import blake2b
-from . import ed25519_blake2b
+from ed25519_blake2b import BadSignatureError, SigningKey, VerifyingKey
 from binascii import hexlify, unhexlify
+from .exceptions import InvalidPrivateKey, InvalidAccount, InvalidSeed, InvalidPublicKey, InvalidQLCAddress, InvalidSignature
+from .helper import is_hex, dec_to_hex
 
+ADDRESSPREFIX = "qlc_"
+ADDRESSLEN = 60
+ADDRESSPREFIXLEN = len(ADDRESSPREFIX)
+HEXADDRESSLENGHT = ADDRESSPREFIXLEN + ADDRESSLEN
+PUBLICKEYSIZEINBYTES = 32
 
-maketrans = hasattr(bytes, 'maketrans') and bytes.maketrans or string.maketrans
+maketrans = hasattr(bytes, 'maketrans') and bytes.maketrans 
 B32_ALPHABET = b'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567'
 QLC_ALPHABET = b"13456789abcdefghijkmnopqrstuwxyz"
 QLC_ENCODE_TRANS = maketrans(B32_ALPHABET, QLC_ALPHABET)
@@ -21,6 +27,7 @@ def address_checksum(address : bytes):
     h.update(address_bytes)
     checksum = bytearray(h.digest())
     checksum.reverse()
+
     return checksum
 
 def private_to_public_key(private_key : bytes):
@@ -29,26 +36,62 @@ def private_to_public_key(private_key : bytes):
     :param private_key: private key (in bytes) to get public key for
     :type private_key: bytes
     """
-    return ed25519_blake2b.publickey_unsafe(private_key)
+    validate_private_key(private_key)
 
-def keypair_from_seed(seed : str, index : int = 0):
+    signing_key = SigningKey(unhexlify(private_key))
+    pub_key = signing_key.get_verifying_key().to_bytes().hex()
+
+    validate_public_key(pub_key)
+
+    return pub_key
+
+def generate_private_key(seed : str, index : int) -> str:
     """
-    Generates a deterministic keypair from `seed` based on `index`
-    :param seed: str value of seed
-    :type seed: bytes
-    :param index: offset from seed
-    :type index: int
-    :return: dict of the form: {
-        'private': private_key
-        'public': public_key
-    }
+    Generate account's private key from a 32-byte seed and index
+    :param str seed: Seed as a 64-character hex string
+    :param int index: Index of the account private key to generate
+    :raises InvalidSeed: If the seed is invalid
+    :raises ValueError: If the index isn't an integer
+    :return: Account private key as a 64-character hex string
+    :rtype: str
     """
-    seed = unhexlify(seed)
-    h = blake2b(digest_size=32)
-    h.update(seed + struct.pack(">L", index))
-    priv_key = h.digest()
-    pub_key = private_to_public_key(priv_key)
-    return {'private': priv_key, 'public': pub_key}
+    validate_seed(seed)
+    if not isinstance(index, int):
+        raise ValueError("Index must be an integer")        
+    acc_bytes = unhexlify(dec_to_hex(index, 4))
+    ctx = blake2b(digest_size=PUBLICKEYSIZEINBYTES)
+    ctx.update(unhexlify(seed))
+    ctx.update(acc_bytes)
+    pk = ctx.hexdigest()
+
+    validate_private_key(pk)
+
+    return pk
+
+def public_key_to_address(public_key : bytes):
+    if type(public_key) != bytes:
+        public_key = unhexlify(public_key)
+        if not len(public_key) == PUBLICKEYSIZEINBYTES:
+            raise InvalidPublicKey('public key must be 32 chars')
+
+    padded = b'000' + public_key
+    address = b32qlc_encode(padded)[4:]
+    checksum = b32qlc_encode(address_checksum(public_key))
+
+    return ADDRESSPREFIX + address.decode('utf-8') + checksum.decode('utf-8')
+
+def address_to_public_key(address : str):
+    validate_qlc_address(address)
+
+    address = bytes(address, "utf-8")
+    key_b32qlc = b'1111' + address[4:56]
+    key_bytes = b32qlc_decode(key_b32qlc)[3:]
+    checksum = address[56:]
+
+    if b32qlc_encode(address_checksum(key_bytes)) != checksum:
+        raise InvalidQLCAddress(f"invalid address, invalid checksum: {address}")
+
+    return hexlify(key_bytes)
 
 def b32qlc_encode(value):
     """
@@ -88,23 +131,86 @@ def verify_signature(message, signature, public_key):
     :return: True if valid, False otherwise
     :rtype: bool
     """
+    validate_public_key(public_key)
 
+    vk = VerifyingKey(unhexlify(public_key))
     try:
-        ed25519_blake2b.checkvalid(signature, message, public_key)
-    except ed25519_blake2b.SignatureMismatch:
-        return False
-    return True
+        vk.verify(
+            sig=unhexlify(signature),
+            msg=unhexlify(message)
+        )
+    except BadSignatureError:
+        raise InvalidSignature("Signature couldn't be verified")
 
-#def __sign_message(message, private_key, public_key):
-#    """
-#    Signs a `message` using `private_key` and `public_key`
-#    .. warning:: Not safe to use with secret keys or secret data. See module
-#                 docstring.  This function should be used for testing only.
-#    :param message: the message to sign
-#    :type message: bytes
-#    :param private_key: private key used to sign message
-#    :type private_key: bytes
-#    :return: the signature of the signed message
-#    :rtype: bytes
-#    """
-#    return ed25519_blake2.signature_unsafe(message, private_key, public_key)
+def sign(message, private_key):
+    """
+    Signs a `message` using `private_key` and `public_key`
+    .. warning:: Not safe to use with secret keys or secret data. See module
+                 docstring.  This function should be used for testing only.
+    :param message: the message to sign
+    :type message: bytes
+    :param private_key: private key used to sign message
+    :type private_key: bytes
+    :return: the signature of the signed message
+    :rtype: bytes
+    """
+    validate_private_key(private_key)
+
+    sk = SigningKey(unhexlify(private_key))
+    sig = sk.sign(msg=unhexlify(message))
+    signature = hexlify(sig).decode()
+
+    return signature
+
+def validate_private_key(private_key):
+    """
+    Validate the given private key and raise an exception on failure
+    :param str private_key: Private key as a 64-character hex string
+    :raises InvalidPrivateKey: If the private key is invalid
+    :return: The private key
+    :rtype: str
+    """  
+    if len(private_key) != 64 or not is_hex(private_key):
+        raise InvalidPrivateKey("Account private key must be a 64-character hexadecimal string")
+    
+    return private_key
+
+def validate_public_key(public_key):
+    """
+    Validate the given public key and raise an exception on failure
+    :param str public_key: Public key as a 64-character hex string
+    :raises InvalidPublicKey: If the public key is invalid
+    :return: The public key
+    :rtype: str
+    """
+    if len(public_key) != 64 or not is_hex(public_key):
+        raise InvalidPublicKey("Account public key must be a 64-character hexadecimal string")
+    return public_key
+
+def validate_seed(seed):
+    """
+    Validate a QLC seed and raise an exception on failure
+    :param str seed: Seed as a 64-character hex string
+    :raises InvalidSeed: If the seed is invalid
+    :return: The seed
+    :rtype: str
+    """
+    seed = str(seed)
+    if len(seed) != 64 or not is_hex(seed):
+        raise InvalidSeed("Seed must be a 64-character hexadecimal string")
+    return seed
+
+def validate_qlc_address(QLC_address : str):
+    """
+    Validate a QLC seed and raise an exception on failure
+    :param str seed: Seed as a 64-character hex string
+    :raises InvalidSeed: If the seed is invalid
+    :return: The seed
+    :rtype: str
+    """
+    try : 
+        address_to_public_key(QLC_address)
+        return True
+    except :
+        return False
+    
